e90ed7d271649370344ed739fd65f9102e083065
LoicSo <souchon.loic@free.fr>
2019-03-29 09:28:09 +0100

    début impl

--- ricm.web.GIVEN/impl/httpserver/itf/impl/HttpStaticRequest.java
+++ ricm.web.GIVEN/impl/httpserver/itf/impl/HttpStaticRequest.java
@@ -18,7 +18,42 @@ public class HttpStaticRequest extends HttpRequest {
 	}
 	
 	public void process(HttpResponse resp) throws Exception {
-	// TO COMPLETE
+		File m_file = null;	// asked file
+		FileInputStream fis = null;	
+		PrintStream ps = null;
+		File folder = m_hs.getFolder();	// folder where we can found the file
+
+		try {
+			m_file = new File(folder + m_ressname);
+			fis = new FileInputStream(m_file);
+			
+			// We found the file and send it
+			resp.setReplyOk();
+			resp.setContentLength((int) m_file.length());
+			resp.setContentType(m_ressname);
+			ps = resp.beginBody();
+			sendFile(ps, fis);
+			
+		} catch (FileNotFoundException e) {
+			// We didn't find the file and send an error
+			resp.setReplyError(404, e.toString());
+		}
+		
+	}
+
+	private void sendFile(PrintStream ps, FileInputStream fis) {
+		int n = 0;
+		byte[] buffer = new byte[BUF_SZ];
+		
+		try {
+			while((n = fis.read(buffer)) >= 0) {
+				
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
 	}
 
 }
